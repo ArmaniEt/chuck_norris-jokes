056a8c13b91798afd7a16df0929ce59d729f96c0
armani <razzarioa@gmail.com>
2019-02-21 14:37:58 +0600

    Added network request to Chuck Norris jokes api and set them to state, map jokes array to pass joke to a component

--- src/container/Jokes.js
+++ src/container/Jokes.js
@@ -13,14 +13,33 @@ export default class Jokes extends Component {
     };
 
     componentDidMount() {
+        const JOKES_URL = 'https://api.chucknorris.io/jokes/random';
+        const N = 5; // amount of jokes
+        let chuckJokes = [];
 
+        for (let i = 0; chuckJokes.length < N; i++){
+            const promises = fetch(JOKES_URL).then(response => {
+                if (response.ok) return response.json();
+                throw new Error("Network request is failed");
+            });
+            chuckJokes.push(promises); // push every promise in a list
+        }
+
+        Promise.all(chuckJokes).then(jokes => {
+            let receivedJokes = jokes.map(joke => {return{...joke}});
+
+            this.setState({jokes: receivedJokes});
+        }).catch(error => {console.log(error)}); // show an error to console
     }
 
 
     render(){
         return(
-            <div>
-
+            <div className="jokes__wrapper">
+                {this.state.jokes.map(joke => <Joke
+                    joke={joke.value} // cause we received an object, we should get a value of this object (joke itself)
+                    />
+                )}
             </div>
         )
     }
